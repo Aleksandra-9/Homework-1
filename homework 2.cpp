1608d327ef97617c3f7c802bf7aa9b5aa2f8504a
Aleksandra-9 <115452298+Aleksandra-9@users.noreply.github.com>
2022-10-10 17:21:30 +0900

    Add files via upload

---
+++ homework 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+ setlocale(LC_ALL, "");
+ int x = 150; // Задание 1.
+ float i = 15.933; // Задание 2.
+ int z = 250; // Задание 3.
+ int d = 11;
+ int m = 7;
+ int y = 2004;
+ double P = 2.3;
+ std::cout << "int = " << x << std::endl; // Задание 4.
+ std::cout << "float = " << i << std::endl; // Задание 4.
+ std::cout << "int = " << z << std::endl; // Задание 4.
+
+ std::cout << "Моя дата рождения: " << d << "." << m << "." << y<< " года" << std::endl;
+ std::cout << "WINDOWS: "<< P << std::endl;
+}
